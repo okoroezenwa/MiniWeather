13069bb1bb9f54dd556c95782e4657b69b6a6b7d
Ezenwa Okoro <okoroezenwa@gmail.com>
2024-03-15 17:26:27 +0100

    Reworked network service tests

--- MiniWeatherTests/Component Tests/Network/StandardNetworkServiceTests.swift
+++ MiniWeatherTests/Component Tests/Network/StandardNetworkServiceTests.swift
@@ -9,73 +9,72 @@ import XCTest
 @testable import MiniWeather
 
 final class StandardNetworkServiceTests: XCTestCase {
+    private var mockSession: URLSession!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         URLProtocol.registerClass(MockURLProtocol.self)
+        
+        let mockConfiguration = URLSessionConfiguration.default
+        mockConfiguration.protocolClasses?.insert(MockURLProtocol.self, at: 0)
+        mockSession = URLSession(configuration: mockConfiguration)
     }
     
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         URLProtocol.unregisterClass(MockURLProtocol.self)
+        mockSession = nil
     }
     
-    func test_onGetDataCall_DataIsReturnedAndNoErrorThrown() async throws {
+    func test_StandardNetworkService_getData_ShouldReturnData() async throws {
+        // Given
         let mockRequest = MockRequest(sessionReturnType: .success)
         let url = try mockRequest.createURLRequest().url!
-        
         MockURLProtocol.stub(url: url, data: "success".data(using: .utf8), response: HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil), error: nil)
         
-        let mockConfiguration = URLSessionConfiguration.default
-        mockConfiguration.protocolClasses?.insert(MockURLProtocol.self, at: 0)
-        let mockSession = URLSession(configuration: mockConfiguration)
-        
+        // When
         let sut = StandardNetworkService(urlSession: mockSession)
+        
+        // Then
         await XCTAssertNoThrowAsync(try await sut.getData(from: mockRequest), "An error was thrown")
     }
     
-    func test_onGetDataCall_ErrorIsReturned() async throws {
+    func test_StandardNetworkService_getData_shouldReturnBadURLError() async throws {
+        // Given
         let mockRequest = MockRequest(sessionReturnType: .error)
         let url = try mockRequest.createURLRequest().url!
-        
         MockURLProtocol.stub(url: url, data: nil, response: nil, error: NetworkError.badURL)
         
-        let mockConfiguration = URLSessionConfiguration.default
-        mockConfiguration.protocolClasses?.insert(MockURLProtocol.self, at: 0)
-        let mockSession = URLSession(configuration: mockConfiguration)
-        
+        // When
         let sut = StandardNetworkService(urlSession: mockSession)
+        
+        // Then
         await XCTAssertThrowsAsyncError(try await sut.getData(from: mockRequest)/*, NetworkError.badURL*/)
     }
     
-    func test_onGetDataCall_OperationFails() async throws {
+    func test_StandardNetworkService_getData_shouldReturnOperationFailedError() async throws {
+        // Given
         let mockRequest = MockRequest(sessionReturnType: .failure)
         let url = try mockRequest.createURLRequest().url!
-        
         MockURLProtocol.stub(url: url, data: nil, response: nil, error: NetworkError.operationFailed)
         
-        let mockConfiguration = URLSessionConfiguration.default
-        mockConfiguration.protocolClasses?.insert(MockURLProtocol.self, at: 0)
-        let mockSession = URLSession(configuration: mockConfiguration)
-        
+        // When
         let sut = StandardNetworkService(urlSession: mockSession)
+        
+        // Then
         await XCTAssertThrowsAsyncError(try await sut.getData(from: mockRequest)/*, NetworkError.operationFailed*/)
     }
     
-    func test_onGetDataCall_InvalidOperationErrorThrown() async throws {
+    func test_StandardNetworkService_getData_shouldThrowInvalidOperationError() async throws {
+        // Given
         let mockRequest = MockRequest(sessionReturnType: .invalidResponse)
-        
-        guard let url = try? mockRequest.createURLRequest().url else {
-            XCTFail("Expected non-nil URL")
-            return
-        }
-        
+        let url = try mockRequest.createURLRequest().url!
         MockURLProtocol.stub(url: url, data: "success".data(using: .utf8), response: HTTPURLResponse(url: url, statusCode: 401, httpVersion: nil, headerFields: nil), error: nil)
         
-        let mockConfiguration = URLSessionConfiguration.default
-        mockConfiguration.protocolClasses?.insert(MockURLProtocol.self, at: 0)
-        let mockSession = URLSession(configuration: mockConfiguration)
-        
+        // When
         let sut = StandardNetworkService(urlSession: mockSession)
+        
+        // Then
         await XCTAssertThrowsAsyncError(try await sut.getData(from: mockRequest)/*, NetworkError.operationFailed*/)
     }
 }
